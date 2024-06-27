19e3faaab74381570bce6bf6b8e1def04f5ddef2
Renato Alves Junior <36175588+renatoalvesjr@users.noreply.github.com>
2024-06-27 13:31:22 -0300

    fix

--- src/Pages/SmartChoices/index.jsx
+++ src/Pages/SmartChoices/index.jsx
@@ -32,29 +32,29 @@ export default function SmartChoices() {
   const [gH, setGH] = useState(0);
   const [gU, setGU] = useState(0);
 
-  const calc = ()=>{
-    
-    var taxaH  = alimentosSaudaveis[choice].kcal / alimentosSaudaveis[choice].peso_gramas;
-    var taxaU  = alimentosNSaudaveis[choiceU].kcal / alimentosNSaudaveis[choiceU].peso_gramas;
+  const calc = () => {
+
+    var taxaH = alimentosSaudaveis[choice].kcal / alimentosSaudaveis[choice].peso_gramas;
+    var taxaU = alimentosNSaudaveis[choiceU].kcal / alimentosNSaudaveis[choiceU].peso_gramas;
     //descobrindo o total de calorias
     var kcalTotal = taxaU * n;
     //descobrindo a quantidade de gramas de alimentos que poderia ser comida no lugar
     var peso = kcalTotal / taxaH;
     setGH(peso);
     setGU(n);
-    
+
 
   }
-  const alterar=(i)=>{
+  const alterar = (i) => {
     setChoiceU(i)
     calc();
   }
-  const alterarH=(i)=>{
+  const alterarH = (i) => {
     setChoice(i)
     calc();
   }
-  
-  useEffect(() => {calc()}, [choiceU, choice, gH, gU,n]);
+
+  useEffect(() => { calc() }, [choiceU, choice, gH, gU, n]);
   return (
     <section style={{ backgroundColor: "#eee" }}>
       <Header />
@@ -67,9 +67,9 @@ export default function SmartChoices() {
 
         <MDBRow className="justify-content-center">
           <MDBCol lg="6" className="">
-            <MDBCard className="my-5 p-2 card">
+            <MDBCard className="my-5 p-2 smart-card">
               <MDBCardBody className="card-body">
-              <MDBCardText className="fonte">Escolha a comida</MDBCardText>
+                <MDBCardText className="fonte">Escolha a comida</MDBCardText>
                 <DropdownButton
                   id="dropdown-item-button"
                   className="my-2 dropdown"
@@ -79,35 +79,35 @@ export default function SmartChoices() {
                     <Dropdown.Item
                       as="button"
                       key={i}
-                      onClick={() => { alterar(i);}}
+                      onClick={() => { alterar(i); }}
                     >
                       {alimento.nome}
                     </Dropdown.Item>
                   ))}
                 </DropdownButton>
-                <MDBInput className="input" type="number" onChange={(e) => setN(e.target.value)} placeholder="Digite o peso em gramas do alimento"/>
+                <MDBInput className="input" type="number" onChange={(e) => setN(e.target.value)} placeholder="Digite o peso em gramas do alimento" />
                 <MDBCardImage
                   className="img my-3"
                   src={alimentosNSaudaveis[choiceU].img}
                 ></MDBCardImage>
-                
+
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
           <MDBCol lg="6" className="">
-            <MDBCard className="my-5 p-2 card">
+            <MDBCard className="my-5 p-2 smart-card">
               <MDBCardBody className="card-body">
-              <MDBCardText className="fonte"> 
-                {
-                gH > 700? 
-                "Você poderia se alimentar com "+Number(gH/1000).toFixed(2)+"kg de "+alimentosSaudaveis[choice].nome+" em vez de "+gU+"g de "+alimentosNSaudaveis[choiceU].nome
-                :"Você poderia se alimentar com "+Math.round(gH)+"g de "+alimentosSaudaveis[choice].nome+" em vez de "+gU+"g de "+alimentosNSaudaveis[choiceU].nome
-          
-                }
-                
-                
-              </MDBCardText>
-                
+                <MDBCardText className="fonte">
+                  {
+                    gH > 700 ?
+                      "Você poderia se alimentar com " + Number(gH / 1000).toFixed(2) + "kg de " + alimentosSaudaveis[choice].nome + " em vez de " + gU + "g de " + alimentosNSaudaveis[choiceU].nome
+                      : "Você poderia se alimentar com " + Math.round(gH) + "g de " + alimentosSaudaveis[choice].nome + " em vez de " + gU + "g de " + alimentosNSaudaveis[choiceU].nome
+
+                  }
+
+
+                </MDBCardText>
+
                 <DropdownButton
                   id="dropdown-item-button"
                   className="my-2 dropdown"
@@ -117,18 +117,18 @@ export default function SmartChoices() {
                     <Dropdown.Item
                       as="button"
                       key={i}
-                      onClick={() => { alterarH(i);}}
+                      onClick={() => { alterarH(i); }}
                     >
                       {alimento.nome}
                     </Dropdown.Item>
                   ))}
                 </DropdownButton>
-               <MDBCardImage
+                <MDBCardImage
                   className="img my-3 rounded"
                   src={alimentosSaudaveis[choice].img}
                 ></MDBCardImage>
-                
-               
+
+
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
