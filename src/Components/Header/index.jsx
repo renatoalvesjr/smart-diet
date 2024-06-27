6aebf0fc7eec57597fd37189835f41c68bb365a6
Renato Alves Junior <36175588+renatoalvesjr@users.noreply.github.com>
2024-06-27 16:16:06 -0300

    nagivate dom

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -6,8 +6,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import Figure from 'react-bootstrap/Figure';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+    const navigate = useNavigate();
     return (
         <Navbar collapseOnSelect expand="md" className="bg-body-tertiary" data-bs-theme="dark">
             <Container>
@@ -15,14 +17,14 @@ export default function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/feed">Feed</Nav.Link>
-                        <Nav.Link href="/progresso">Progresso</Nav.Link>
-                        <Nav.Link href="/ranking">Ranking</Nav.Link>
-                        <Nav.Link href="/smartchoices">SmartChoices</Nav.Link>
+                        <Nav.Link onClick={() => navigate("/feed")}>Feed</Nav.Link>
+                        <Nav.Link onClick={() => navigate("/progresso")}>Progresso</Nav.Link>
+                        <Nav.Link onClick={() => navigate("/ranking")}>Ranking</Nav.Link>
+                        <Nav.Link onClick={() => navigate("/smartchoices")}>SmartChoices</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="/profile">Meu Perfil</Nav.Link>
-                        <Nav.Link href="/profile">
+                        <Nav.Link onClick={() => navigate("/profile")}>Meu Perfil</Nav.Link>
+                        <Nav.Link onClick={() => navigate("/profile")}>
                             <Figure.Image
                                 width={25}
                                 height={25}
